6bf0325fc3d10aeeb831a1004b979536d1230336
dariomicelii <dariomiceli01@gmail.com>
2024-10-18 15:00:21 +0200

    palindroma

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
-userWord = prompt("Inserisci una parola per capire se è palindroma o no:")
+userWord = prompt("Inserisci una parola per capire se è palindroma o no ;)")
 
 function reverseWord(userWord){
     return userWord.split('').reverse().join('');
